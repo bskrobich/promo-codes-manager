3dcc29049ade01caca435b2f66d8a38390868ba3
bskrobich <bartskrobich@gmail.com>
2024-05-10 18:48:51 +0200

    Initial commit

---
+++ src/main/java/com/bskrobich/promocodesmanager/PromoCodesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.bskrobich.promocodesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoCodesManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromoCodesManagerApplication.class, args);
+    }
+
+}
