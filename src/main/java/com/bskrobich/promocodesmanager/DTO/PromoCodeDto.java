82f7ebb9c6495d9448c2d6b77c1d28d85375d0d1
bskrobich <bartskrobich@gmail.com>
2024-05-11 15:43:55 +0200

    Added @Builder

--- src/main/java/com/bskrobich/promocodesmanager/DTO/PromoCodeDto.java
+++ src/main/java/com/bskrobich/promocodesmanager/DTO/PromoCodeDto.java
@@ -1,6 +1,7 @@
 package com.bskrobich.promocodesmanager.DTO;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class PromoCodeDto {
     private String code;
     private LocalDate expirationDate;
