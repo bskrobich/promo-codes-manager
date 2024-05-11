004e0ce44ca2b3c06df0f79bdb964dfc14d301d0
bskrobich <bartskrobich@gmail.com>
2024-05-11 15:02:33 +0200

    Added models

---
+++ src/main/java/com/bskrobich/promocodesmanager/Model/Product.java
@@ -0,0 +1,44 @@
+package com.bskrobich.promocodesmanager.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long productId;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private BigDecimal regularPrice;
+
+    @Column
+    private String currency;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(productId, product.productId) && Objects.equals(name, product.name) && Objects.equals(description, product.description) && Objects.equals(regularPrice, product.regularPrice) && Objects.equals(currency, product.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, name, description, regularPrice, currency);
+    }
+}
---
+++ src/main/java/com/bskrobich/promocodesmanager/Model/PromoCode.java
@@ -0,0 +1,47 @@
+package com.bskrobich.promocodesmanager.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PromoCode {
+    @Id
+    @Column
+    private String code;
+
+    @Column
+    private LocalDate expirationDate;
+
+    @Column
+    private BigDecimal discountAmount;
+
+    @Column
+    private String currency;
+
+    @Column
+    private int maxUsages;
+
+    @Column
+    private int numberOfUsages;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PromoCode promoCode = (PromoCode) o;
+        return maxUsages == promoCode.maxUsages && numberOfUsages == promoCode.numberOfUsages && Objects.equals(code, promoCode.code) && Objects.equals(expirationDate, promoCode.expirationDate) && Objects.equals(discountAmount, promoCode.discountAmount) && Objects.equals(currency, promoCode.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, expirationDate, discountAmount, currency, maxUsages, numberOfUsages);
+    }
+}
