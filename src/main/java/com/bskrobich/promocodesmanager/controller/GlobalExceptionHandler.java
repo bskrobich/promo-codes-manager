8bf08ef9a3faf1f2453ef5d4cbc56319f4c60030
bskrobich <bartskrobich@gmail.com>
2024-05-12 14:53:18 +0200

    Added GlobalExceptionHandler

---
+++ src/main/java/com/bskrobich/promocodesmanager/controller/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.bskrobich.promocodesmanager.controller;
+
+import com.bskrobich.promocodesmanager.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponseDto> handleIllegalArgumentException(Exception e) {
+
+        ErrorResponseDto dto = ErrorResponseDto.builder()
+                .errorMessage(e.getMessage())
+                .timestamp(LocalDateTime.now())
+                .code(HttpStatus.BAD_REQUEST.value())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(dto);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Void> handleNoSuchElementException() {
+        return ResponseEntity.notFound().build();
+    }
+}
